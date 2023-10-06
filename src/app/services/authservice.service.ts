88e110288a93f39f9521591303b975fe9bba45b9
Mario Teixeira <mariomarcio12324@gmail.com>
2023-10-06 00:32:50 -0300

    Parte principal do backend com os filtros principais do projeto, adicionado README

---
+++ src/app/services/authservice.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map, Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiURL = 'https://gsm-hmg.centralitcloud.com.br/citsmart/services/login';
+  private sessionToken: string = '';
+
+  constructor(private http: HttpClient) {
+    this.sessionToken = localStorage.getItem('sessionToken') || '';
+   }
+
+  login(userName: string, password: string): Observable<any> {
+    const fullUserName = 'citsmart.local\\' + userName;
+
+    const body = {
+      clientID: 'API_PBI',
+      language: 'pt_BR',
+      userName: fullUserName,
+      password: password,
+    }
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    
+    return this.http.post(this.apiURL, body, { headers }).pipe(
+      map((response: any) => {
+        if (response.sessionID) {
+          this.sessionToken = response.sessionID;
+          localStorage.setItem('sessionToken', this.sessionToken);
+          this.startSessionTimer();
+        }
+        return response;
+      })
+    );
+  }
+  startSessionTimer() {
+    setTimeout(() => {
+      this.sessionToken = '';
+      localStorage.removeItem('sessionToken');
+      alert('Sua sessão expirou. Por favor, faça login novamente.');
+    }, 10 * 60 * 1000);
+  }
+  getSessionToken(): string {
+    return this.sessionToken;
+  }
+}
